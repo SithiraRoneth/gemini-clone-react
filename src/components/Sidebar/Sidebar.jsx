23fa6ab94670d3475bb0f0d9e45dd3e2d454ffee
USER <sithiraroneth@gmail.com>
2025-01-21 00:50:38 +0530

    delete some files

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import {useContext, useState} from 'react';
 import '../Sidebar/Sidebar.css'
 import {assets} from "../../assets/assets.js";
 import {Context} from "../../context/Context.jsx";
